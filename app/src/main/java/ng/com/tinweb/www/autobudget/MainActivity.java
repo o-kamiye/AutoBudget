2728a985051d1602aa6b006adc2fae97d101c164
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@gmail.com>
2016-10-23 16:06:46 +0100

    Design landing page

--- app/src/main/java/ng/com/tinweb/www/autobudget/MainActivity.java
+++ app/src/main/java/ng/com/tinweb/www/autobudget/MainActivity.java
@@ -1,5 +1,6 @@
 package ng.com.tinweb.www.autobudget;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,23 +10,16 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import ng.com.tinweb.www.autobudget.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        activityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 
     @Override
