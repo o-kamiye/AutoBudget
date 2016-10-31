d4067dc8be99e1a6742fb5ca2b3194b0fb919520
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@gmail.com>
2016-10-31 12:17:09 +0100

    set home button to return to dashboard activity in settings

--- app/src/main/java/ng/com/tinweb/www/autobudget/SettingsActivity.java
+++ app/src/main/java/ng/com/tinweb/www/autobudget/SettingsActivity.java
@@ -2,29 +2,29 @@ package ng.com.tinweb.www.autobudget;
 
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setupActionBar();
         setContentView(R.layout.user_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getFragmentManager().beginTransaction()
                 .replace(R.id.settings_container, new SettingsFragment())
                 .commit();
     }
 
-    /**
-     * Set up the {@link android.app.ActionBar}, if the API is available.
-     */
-    private void setupActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            // Show the Up button in the action bar.
-            actionBar.setDisplayHomeAsUpEnabled(true);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
         }
+        return super.onOptionsItemSelected(item);
     }
+
 }
